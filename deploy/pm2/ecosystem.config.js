7d4e2fab2cf42fb80ec5e2858fb14ae0cac6b132
tobenot <42083410+tobenot@users.noreply.github.com>
2025-09-03 21:07:21 +0800

    fix: env publish

--- deploy/pm2/ecosystem.config.js
+++ deploy/pm2/ecosystem.config.js
@@ -4,13 +4,15 @@ module.exports = {
   apps: [
     {
       name: pkg.name.replace(/^@.*\//, ''), // must match APP in scripts if you use pm2 commands by name
-      script: './bin/start',
+      script: 'dist/server.js',
+      node_args: '-r dotenv/config',
       cwd: '.',
       instances: 1, // set to 2 or 'max' for zero-downtime reloads
       exec_mode: 'cluster',
       max_memory_restart: '512M',
       env: {
         NODE_ENV: 'production',
+        DOTENV_CONFIG_PATH: './.env.publish',
       },
       out_file: `/var/log/${pkg.name.replace(/^@.*\//, '')}/out.log`,
       error_file: `/var/log/${pkg.name.replace(/^@.*\//, '')}/err.log`,
